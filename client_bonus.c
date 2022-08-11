53ca8e4309a032dce8cb3536bd2d8802d1ef629d
Cristobal Gonzalez aguilar <crigonza@c3r9s2.42malaga.com>
2022-08-11 13:45:22 +0200

    42

--- client_bonus.c
+++ client_bonus.c
@@ -6,57 +6,56 @@
 /*   By: crigonza <crigonza@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/02 20:42:23 by crigonza          #+#    #+#             */
-/*   Updated: 2022/08/05 18:11:17 by crigonza         ###   ########.fr       */
+/*   Updated: 2022/08/11 13:23:06 by crigonza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "./Libft/libft.h"
+#include <signal.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <signal.h>
-#include "./Libft/libft.h"
 
-void    handler(int signal)
+void	handler(int signal)
 {
-    if (signal == SIGUSR1)
-        ft_putstr_fd("ACK  ", 1);
+	if (signal == SIGUSR1)
+		ft_putstr_fd("ACK  ", 1);
 }
 
-void    send_bit(int pid, unsigned char byte)
+void	send_bit(int pid, unsigned char byte)
 {
-    int bpos;
+	int	bpos;
 
-    bpos = 0;
-    while (bpos < 8)
-    {
-        if ((byte >> bpos)& 1)
-            kill(pid, SIGUSR1);
-        else
-            kill(pid, SIGUSR2);
-        bpos++;
-        usleep(50);
-    }
+	bpos = 0;
+	while (bpos < 8)
+	{
+		if ((byte >> bpos) & 1)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		bpos++;
+		usleep(50);
+	}
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    struct sigaction    sig;
-    int                 i;
+	struct sigaction	sig;
+	int					i;
 
-    i = 0;
-    if (argc != 3)
-    {
-        ft_putstr_fd("Error.Invalid Arguments", 2);
-        exit(EXIT_FAILURE);
-    }
-    sig.sa_handler = handler;
-    sig.sa_flags = 0;
-    sigemptyset(&sig.sa_mask);
-    sigaction(SIGUSR1, &sig, NULL);
-    while (argv[2][i])
-    {
-        send_bit(ft_atoi(argv[1]), (unsigned char)argv[2][i]);
-        i++;
-    }
-    return(0);
+	i = 0;
+	if (argc != 3)
+	{
+		ft_putstr_fd("Error.Invalid Arguments", 2);
+		exit(EXIT_FAILURE);
+	}
+	sig.sa_handler = handler;
+	sig.sa_flags = 0;
+	sigemptyset(&sig.sa_mask);
+	sigaction(SIGUSR1, &sig, NULL);
+	while (argv[2][i])
+	{
+		send_bit(ft_atoi(argv[1]), (unsigned char)argv[2][i]);
+		i++;
+	}
+	return (0);
 }
-
--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: crigonza <crigonza@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 19:50:44 by crigonza          #+#    #+#             */
-/*   Updated: 2022/08/05 18:11:14 by crigonza         ###   ########.fr       */
+/*   Updated: 2022/08/11 13:37:44 by crigonza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,37 +15,37 @@
 #include <signal.h>
 #include "./Libft/libft.h"
 
-void    handler(int signal)
+void	handler(int signal)
 {
-    static char c = 0;
-    static int  bpos = 0;
+	static char	c = 0;
+	static int	bpos = 0;
 
-    if( signal == SIGUSR1)
-        c += 1 << bpos;
-    else if (signal == SIGUSR2)
-        c += 0 << bpos;
-    bpos ++;
-    if (bpos == 8)
-    {
-        ft_putchar_fd(c, 1);
-        c = 0;
-        bpos = 0;
-    }
+	if (signal == SIGUSR1)
+		c += 1 << bpos;
+	else if (signal == SIGUSR2)
+		c += 0 << bpos;
+	bpos ++;
+	if (bpos == 8)
+	{
+		ft_putchar_fd(c, 1);
+		c = 0;
+		bpos = 0;
+	}
 }
 
-int main(void)
+int	main(void)
 {
-    struct sigaction    sact;
+	struct sigaction	sact;
 
-    sact.sa_handler = handler;
-    sact.sa_flags = 0;
-    sigemptyset(&sact.sa_mask);
-    sigaction(SIGUSR1, &sact, NULL);
-    sigaction(SIGUSR2, &sact, NULL);
-    ft_putstr_fd("Server PID: ", 1);
-    ft_putnbr_fd(getpid(), 1);
-    write(1, "\n", 1);
-    while (1)
-        pause();
-    return(0);
+	sact.sa_handler = handler;
+	sact.sa_flags = 0;
+	sigemptyset(&sact.sa_mask);
+	sigaction(SIGUSR1, &sact, NULL);
+	sigaction(SIGUSR2, &sact, NULL);
+	ft_putstr_fd("Server PID: ", 1);
+	ft_putnbr_fd(getpid(), 1);
+	write(1, "\n", 1);
+	while (1)
+		pause();
+	return (0);
 }
